a1b170f871b8985c4a08a8b461c00f04dc832aa8
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-26 16:23:43 -0800

    Create index.tsx
    to centralize components

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+import { Nav, navLinks, navLinksMapped } from "./Nav"
+import HomeWelcomeCard from "./HomeWelcomeCard"
+import ProdutShowcase from "./ProductShowcase"
+export { Nav, navLinks, navLinksMapped, HomeWelcomeCard, ProdutShowcase }
