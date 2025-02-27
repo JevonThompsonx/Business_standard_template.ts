aebcb885efc2a413c05313ceebc8d9036416abea
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-27 12:35:54 -0800

    added to dos and adjusted text display

--- src/components/WelcomeCards/HomeWelcomeCardDesktop.tsx
+++ src/components/WelcomeCards/HomeWelcomeCardDesktop.tsx
@@ -1,10 +1,18 @@
 const HomeWelcomeCardDesktop = () => {
   return (
-    <div className="w-[90%] mx-auto h-auto aspect-video bg-[url(/images/Desktop/klara-kulikova-6MxPH_N4huE-unsplash.jpg)] bg-cover bg-center flex flex-row items-center justify-evenly rounded-2xl">
-      <div id="text-box" className="h-full w-2/5 bg-white/60 flex flex-col justify-evenly items-start pl-6">
-        <button className="p-2 bg-white text-black h-14 w-32 rounded-2xl border-1 border-black"> Click me </button>
-        <h1 className="text">hola</h1>
-        <p className="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, non!</p>
+    // At some point I'm prob gonna change the image and adjust how the text is showing but this is good for now
+    // to do: 
+    // [] Choose which image I'll be using  
+    //    [] Something that fits the color scheme 
+    //      [] decide color scheme
+    // [] Choose how the text will appear 
+    //    [] font 
+    //    [] transparency with image
+    <div className="w-[90%] mx-auto h-auto aspect-video bg-[url(/images/Desktop/mor-shani-FIxc0thOzEQ-unsplash.jpg)] bg-cover bg-center flex flex-row items-center justify-evenly rounded-2xl">
+      <div id="text-box" className="h-full w-2/5 bg-white/20 flex flex-col justify-evenly items-start pl-6">
+        <button className="p-1 bg-transparent font-bold text-black h-10 w-36 rounded-2xl border-1 border-black"> Unwind </button>
+        <h1 className="">Lorem, ipsum dolor.</h1>
+        <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo praesentium ipsam doloribus enim sit dicta quisquam neque blanditiis </p>
       </div>
       <div className="h-full w-3/5"></div>
     </div>
