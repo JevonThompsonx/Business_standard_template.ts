751a85f61bc3347c8445bf6dd6e762b708af09c7
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-27 12:21:02 -0800

    Separated desktop and mobile files in folders

--- src/components/WelcomeCards/HomeWelcomeCardDesktop.tsx
+++ src/components/WelcomeCards/HomeWelcomeCardDesktop.tsx
@@ -1,6 +1,6 @@
 const HomeWelcomeCardDesktop = () => {
   return (
-    <div className="w-[90%] mx-auto h-auto aspect-video bg-[url(/images/kaylee-garrett-GaprWyIw66o-unsplash.jpg)] bg-cover bg-center flex flex-col items-center justify-evenly rounded-2xl">
+    <div className="w-[90%] mx-auto h-auto aspect-video bg-[url(/images/Desktop/kaylee-garrett-GaprWyIw66o-unsplash.jpg)] bg-cover bg-center flex flex-col items-center justify-evenly rounded-2xl">
       <div className="h-3/5 w-full"></div>
       <div id="text-box" className="h-3/5 md:h-2/5 w-full bg-white/60 flex flex-col justify-evenly ">
         <h1 className="text">hola</h1>
