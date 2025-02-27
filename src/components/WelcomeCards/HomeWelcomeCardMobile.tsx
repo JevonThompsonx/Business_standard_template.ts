6a35ff9b87c3e76c232a1e52e1a76a7e7ed3b733
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-27 12:15:28 -0800

    Separate welcome displays for mobile and desktop added

---
+++ src/components/WelcomeCards/HomeWelcomeCardMobile.tsx
@@ -0,0 +1,15 @@
+const HomeWelcomeCardMobile = () => {
+  return (
+    <div className="aspect-square w-[90%] h-auto mx-auto  bg-[url(images/erik-brolin-wmZNEGkcsmw-unsplash.jpg)] bg-cover bg-center flex flex-col items-center justify-evenly rounded-2xl">
+      <div className="h-3/5 w-full"></div>
+      <div id="text-box" className="h-3/5 md:h-2/5 w-full bg-white/60 flex flex-col justify-evenly ">
+        <h1 className="text">hola</h1>
+        <p className="text">The mobile one</p>
+        <button className="p-2 bg-white text-black h-14 w-32 rounded-2xl border-1 border-black"> Click me </button>
+      </div>
+    </div>
+  )
+
+}
+
+export default HomeWelcomeCardMobile
