d6dd1bdbabeab6d1abea523d9d9b2174fc74f766
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-26 16:38:35 -0800

    Diff styles diff sizes
    Updated welcome card to have responsive styles

--- src/components/HomeWelcomeCard.tsx
+++ src/components/HomeWelcomeCard.tsx
@@ -1,10 +1,10 @@
 const HomeWelcomeCard = () => {
   return (
-    <div className="w-full aspect-video mx-6 bg-[url(/images/kaylee-garrett-GaprWyIw66o-unsplash.jpg)] bg-cover bg-center flex flex-col items-center justify-evenly">
+    <div className="w-[90%] h-auto aspect-square md:aspect-video bg-[url(images/erik-brolin-wmZNEGkcsmw-unsplash.jpg)] md:bg-[url(/images/kaylee-garrett-GaprWyIw66o-unsplash.jpg)] bg-cover bg-center flex flex-col items-center justify-evenly">
       <div className="h-3/5 w-full"></div>
-      <div id="text-box" className="h-2/5 w-full bg-white/60 flex flex-col">
-        <h1 className="text-2xl">Hola</h1>
-        <p className="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, non!</p>
+      <div id="text-box" className="h-3/5 md:h-2/5 w-full bg-white/60 flex flex-col justify-evenly ">
+        <h1 className="text">hola</h1>
+        <p className="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, non!</p>
         <button className="p-2 bg-white text-black h-14 w-32 rounded-2xl border-1 border-black"> Click me </button>
       </div>
     </div>
