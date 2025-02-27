5e6f1b23471006c204721b0f9cb3029cb53fa385
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-27 13:09:16 -0800

    updated index w/ imports and exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
 import { Nav, navLinks, navLinksMapped } from "./Nav"
 import { HomeWelcomeCardMobile, HomeWelcomeCardDesktop, ReactiveWelcomeCard } from "./WelcomeCards/"
-import ProdutShowcase from "./ProductShowcase"
-export { Nav, navLinks, navLinksMapped, HomeWelcomeCardDesktop, HomeWelcomeCardMobile, ProdutShowcase, ReactiveWelcomeCard }
+import ProductShowcase from "./ShowcaseComponents/ProductShowcase"
+export { Nav, navLinks, navLinksMapped, HomeWelcomeCardDesktop, HomeWelcomeCardMobile, ProductShowcase, ReactiveWelcomeCard }
