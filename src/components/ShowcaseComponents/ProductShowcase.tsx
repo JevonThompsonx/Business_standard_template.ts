46c75513501b3d6eb5a8098e826e5b09880eea8d
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-28 13:07:00 -0800

    added email template

--- src/components/ShowcaseComponents/ProductShowcase.tsx
+++ src/components/ShowcaseComponents/ProductShowcase.tsx
@@ -1,13 +1,14 @@
-import { ProductTemplate } from "./index";
+import { ProductTemplate, ProductTemplateWithEmail } from "./index";
 
 const ProductShowcase = () => {
   // Example data for ComponentAttributeList
   const ComponentAttributeList = [
     { id: 4, ButtonText: "Hi bud", Link: "blahblahlink", Heading: "lorem lorem" },
     { id: 1, ButtonText: "Hi bud", Link: "blahblahlink", Heading: "lorem lorem" },
-    { id: 2, ButtonText: "Hi bud", Link: "blahblahlink", Heading: "lorem lorem" },
   ];
-
+  const ComponentAttributeListForEmailComponent = [
+    { id: 4, ButtonText: "Hi bud", Link: "blahblahlink", Heading: "lorem lorem" },
+  ];
   return (
     <div
       className="
@@ -17,6 +18,7 @@ const ProductShowcase = () => {
     h-fit
     flex
     flex-col
+    space-y-4
     md:flex-row
     md:space-x-2
     md:space-y-0
@@ -25,7 +27,9 @@ const ProductShowcase = () => {
       {ComponentAttributeList.map((product) => (
         <ProductTemplate key={product.id} {...product} />
       ))}
-    </div>
+      {ComponentAttributeListForEmailComponent.map((product) => (
+        <ProductTemplateWithEmail key={product.id} {...product} />
+      ))}    </div>
   );
 };
 
