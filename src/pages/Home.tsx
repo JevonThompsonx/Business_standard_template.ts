34ebc8c559a895f1beb17fd368dea17e74135dfe
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-28 11:23:11 -0800

    sizing fix
    Components were overflowing out of view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <div className="flex flex-col justify-evenly items-center h-screen w-screen bg-white">
+      <div className="flex flex-col justify-evenly items-center min-h-fit w-screen bg-white overflow-auto">
         <ReactiveWelcomeCard />
         <ProductShowcase />
       </div>
