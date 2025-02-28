502538790f3a2cc3e86088ad05d7966efb98a1d7
Jevon Thompson <jthompsonx00@gmail.com>
2025-02-27 17:28:44 -0800

    Used ai to cleanup code
    Responsiveness fine tuned a bit

--- src/components/WelcomeCards/ReactiveWelcomeCard.tsx
+++ src/components/WelcomeCards/ReactiveWelcomeCard.tsx
@@ -20,17 +20,28 @@ const ReactiveWelcomeCard: React.FC = () => {
   // To do: 
   // [] Fix sizing 
   // [] 
+
   return (
-    <div className="aspect-square md:aspect-video w-[90%] md:max-w-[90%] md:h-auto md:max-h-[80%] mx-auto mt-10 md:mt-28 mb-6 md:mb-[5vh]">
+    <div className="aspect-square min-h-56 max-w-[90%] md:aspect-video md:max-w-[90%] md:h-auto md:max-h-[80%] mx-auto mt-10 md:mt-28  md:mb-[5vh] landscape:md:max-h-[90vh]
+               portrait:md:max-h-[75vh]
+     pb-[safe-area-inset-bottom]
+               pl-[safe-area-inset-left]
+               pr-[safe-area-inset-right]"
+      style={{
+        minHeight: 'clamp(224px, 50vh, 400px)',
+        marginTop: 'clamp(2rem, 5vh, 7rem)'
+      }}>
 
-      {isMobile ? (
-        <HomeWelcomeCardMobile />
-      ) : (
-        <HomeWelcomeCardDesktop />
+      {
+        isMobile ? (
+          <HomeWelcomeCardMobile />
+        ) : (
+          <HomeWelcomeCardDesktop />
 
-      )}
-    </div>
-  );
+        )
+      }
+    </div >
+  )
 };
 
 export default ReactiveWelcomeCard;
